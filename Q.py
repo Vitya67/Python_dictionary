7af184c6a7b246d5000f6853daebb5f0c64cf9b2
Vitya67 <kapitansd35@gmail.com>
2024-05-30 15:53:19 +0300

    Q

---
+++ Q.py
@@ -0,0 +1,39 @@
+def find_countries_for_cities(country_data, city_queries):
+    city_to_country = {}
+
+
+    for data in country_data:
+        entries = data.split()
+        country = entries[0]
+        cities = entries[1:]
+        for city in cities:
+            city_to_country[city] = country
+
+
+    results = []
+    for city in city_queries:
+        results.append(city_to_country[city])
+
+    return results
+
+def main():
+    # Вхідні дані
+    country_data = [
+        "Germany Berlin Hamburg Munich Dortmund",
+        "Ukraine Kyiv Lviv Odessa"
+    ]
+    city_queries = [
+        "Odessa",
+        "Dortmund",
+        "Berlin"
+    ]
+
+
+    results = find_countries_for_cities(country_data, city_queries)
+
+
+    for result in results:
+        print(result)
+
+if __name__ == "__main__":
+    main()
